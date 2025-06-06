48ecf010429751db7fb575728b3a223f071051af
BrunoHofstatter <brunohofstatter2025@gmail.com>
2025-06-06 18:26:33 -0300

    Imagens home

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,13 +1,47 @@
 //import MainMenu from "../Components/mainMenu";
 import "../App.css";
-import MudarPagina from "../Components/MudarPagina";
+import { useNavigate } from "react-router-dom";
+
 function Home() {
+  const navigate = useNavigate();
+  const mudar_pagina = (pagina: string) => {
+    navigate(`/${pagina}`);
+  };
   return (
     <>
       <div className="logo">
-        <img src="public/logoTexto+.png" className="logoTexto"  />
-      </div>
-      <MudarPagina />
+  <picture>
+    <source srcSet="public/logoEscritaVertical.png" media="(max-width: 668px)" />
+    <img src="public/logoEscritaHorizontal.png" className="logoTexto" alt="Logo" />
+  </picture>
+</div>
+      <>
+      
+        <div className="buttonsHome">
+          <button
+            className="buttonJogar"
+            onClick={() => mudar_pagina("jogos")}
+          >
+            {" "}
+            Jogar
+          </button>
+          <button
+            className="buttonSobre"
+            onClick={() => mudar_pagina("sobre")}
+          >
+            {" "}
+            Sobre
+          </button>
+          <button
+            className="buttonContato"
+            onClick={() => mudar_pagina("contato")}
+          >
+            {" "}
+            Contato
+          </button>
+        </div>
+      
+    </>
       
     </>
   );
