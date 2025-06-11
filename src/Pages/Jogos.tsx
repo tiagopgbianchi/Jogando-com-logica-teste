c0cc920cb96b332c9fe424ba0f92d224c271a4d4
BrunoHofstatter <brunohofstatter2025@gmail.com>
2025-06-11 19:10:37 -0300

    deu

--- src/Pages/Jogos.tsx
+++ src/Pages/Jogos.tsx
@@ -5,26 +5,18 @@ function Jogos() {
   return (
     <div className="jogosPg">
       <h2>Jogos:</h2>
-<<<<<<< Updated upstream
       <div className="grade_jogos">
         <GameButton
           pagina="jogoStop"
           label="Stop Matemático"
-          imageSrc="public/iconStop.png"
+          imageSrc={`${import.meta.env.BASE_URL}iconStop.png`}
         />
         <GameButton
           pagina="damas"
           label="Jogo de Damas"
-          imageSrc="public/imagem_damas.png"
+          imageSrc={`${import.meta.env.BASE_URL}imagem_damas.png`}
         />
       </div>
-=======
-      <GameButton
-        pagina="jogoStop"
-        label="Stop Matemático"
-        imageSrc={`${import.meta.env.BASE_URL}iconStop.png`}
-      />
->>>>>>> Stashed changes
     </div>
   );
 }
