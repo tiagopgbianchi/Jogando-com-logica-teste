b04c836bde422415e86e48e963e4082c5ff6fa6d
BrunoHofstatter <brunohofstatter2025@gmail.com>
2025-06-24 20:51:41 -0300

    regrasstop csss

--- src/Stop/RegrasStop.tsx
+++ src/Stop/RegrasStop.tsx
@@ -12,40 +12,42 @@ function JogoStop() {
   return (
     <div className="regrasPage">
       {/* Left Side - Rules */}
-      <div className="boxRegras">
-        {/*<h2>Como Jogar Stop Matemático</h2>*/}
-        <ul className="regras">
-          <li>
-            <h3>O Número Mágico vai ser sorteado</h3>
-            <img
-              src={`${import.meta.env.BASE_URL}ComoJogarStop1.png`}
-              className="como c1"
-            />
-          </li>
-          <li>
-            <img
-              src={`${import.meta.env.BASE_URL}ComoJogarStop2.png`}
-              className="como c2"
-            />
-            <h3>Use o Número Mágico para fazer as contas</h3>
-          </li>
-          <li>
-            <h3>Clique no botão STOP quando terminar</h3>
-            <div className="c3Box">
+      <div className="boxBorder">
+        <div className="boxRegras">
+          
+          <ul className="regras">
+            <li>
+              <h3>O Número Mágico vai ser sorteado</h3>
               <img
-                src={`${import.meta.env.BASE_URL}ComoJogarStop3.png`}
-                className="como c3"
+                src={`${import.meta.env.BASE_URL}ComoJogarStop1.png`}
+                className="como c1"
               />
-            </div>
-          </li>
-          <li>
-            <img
-              src={`${import.meta.env.BASE_URL}ComoJogarStop4.png`}
-              className="como c4"
-            />
-            <h3>Veja seu tempo e </h3>
-          </li>
-        </ul>
+            </li>
+            <li>
+              <img
+                src={`${import.meta.env.BASE_URL}ComoJogarStop2.png`}
+                className="como c2"
+              />
+              <h3>Use o Número Mágico para fazer as contas</h3>
+            </li>
+            <li>
+              <h3>Clique no botão STOP quando terminar</h3>
+              <div className="c3Box">
+                <img
+                  src={`${import.meta.env.BASE_URL}ComoJogarStop3.png`}
+                  className="como c3"
+                />
+              </div>
+            </li>
+            <li>
+              <img
+                src={`${import.meta.env.BASE_URL}ComoJogarStop4.png`}
+                className="como c4"
+              />
+              <h3>Veja os acertos e seu tempo </h3>
+            </li>
+          </ul>
+        </div>
       </div>
 
       {/* Right Side - Game Controls */}
@@ -56,7 +58,7 @@ function JogoStop() {
         <div className="difPart">
           <div className="difToggle" onClick={() => setIsOpen(!isOpen)}>
             <h2 className="difLabel">Dificuldade</h2>
-            {isOpen ? "﹀" : "︿"}
+            {isOpen ? <div className="downArrow">-</div> : <div className="upArrow">+</div>}
           </div>
 
           {isOpen && (
