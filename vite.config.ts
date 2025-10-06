ff57e2eb7c9dff764d1d51cf31167ba496c9f42a
tiagopgbianchi <tiagopgbianchi@gmail.com>
2025-10-06 16:00:02 -0300

    ss

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => {
   // command === 'serve' when running `vite` or `npm run dev`
   // command === 'build' when running `vite build` or `npm run build`
-  
+
   return {
-    base: command === 'build' ? '/Jogando-com-logica/' : '/',
+    base: command === "build" ? "/Jogando-com-logica-teste/" : "/",
     plugins: [react()],
-  }
-})
+  };
+});
